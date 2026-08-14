946b8a1ee941a4b6884294887b44181d4c730741
Jakub Tomaszewski <jakubtomaszewski@vp.pl>
2026-08-14 16:44:29 +0200

    add some integration tests

---
+++ tests/Prescriptions.Api.Tests/Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Prescriptions.Api.Data;
+
+namespace Prescriptions.Api.Tests.Integration;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<IDbContextOptionsConfiguration<DatabaseContext>>();
+
+
+            services.AddSingleton<DbConnection>(container =>
+            {
+                var connection = new SqliteConnection("DataSource=:memory:");
+
+                connection.Open();
+                return connection;
+            });
+
+            services.AddDbContext<DatabaseContext>((container, options) =>
+            {
+                var connection = container.GetRequiredService<DbConnection>();
+                options.UseSqlite(connection);
+            });
+        });
+        builder.UseEnvironment("Test");
+    }
+
+    public async Task ResetDatabaseAsync(CancellationToken cancellationToken)
+    {
+        using var scope = Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+        await context.Database.EnsureDeletedAsync(cancellationToken);
+        await context.Database.EnsureCreatedAsync(cancellationToken);
+    }
+    
+}
